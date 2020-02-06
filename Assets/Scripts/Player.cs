bc585cf44d7e1699b96c8c1a781adff6996a4c1d
jfcarocota <jfcarocota@gmail.com>
2020-02-06 12:18:53 -0700

    player move on x

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   
+
+    Vector2 Axis
+    {
+        get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * 5f * Time.deltaTime * Axis.x);
+    }
 }
